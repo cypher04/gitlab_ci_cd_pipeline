98d57a0bc4e02ecc3e528a7aaa3880ca6582c702
Seyi Abiodun <cypher04>
2026-08-29 23:28:13 +0100

    refactor(principal ID): Changed principal ID and Scope of both API Access and Storage access

--- gitlab/roles.tf
+++ gitlab/roles.tf
@@ -3,14 +3,15 @@
 
 // role for service principal to access GitLab API
 resource "azurerm_role_assignment" "gitlab_api_access" {
-  principal_id   = azuread_service_principal.gitlab_sp.id
+  principal_id   = azuread_service_principal.gitlab_sp.object_id
   role_definition_name = "Contributor"
-  scope          = var.gitlab_base_url
+  // resource group scope
+  scope = "/subscriptions/${var.subscription_id}/resourceGroups/cicdprojectdev-rg"
 }
 
 // role for service principal to access storage account for Terraform state
 resource "azurerm_role_assignment" "gitlab_storage_access" {
-  principal_id   = azuread_service_principal.gitlab_sp.id
+  principal_id   = azuread_service_principal.gitlab_sp.object_id
   role_definition_name = "Storage Blob Data Contributor"
   scope          = "/subscriptions/${var.subscription_id}/resourceGroups/cicdprojectdev-rg/providers/Microsoft.Storage/storageAccounts/cicdprojectstatedev"
 }
