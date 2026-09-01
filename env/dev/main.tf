28946a433335b9371d2946be55a99d7624bee0ba
Seyi Abiodun <cypher04>
2026-09-01 06:29:43 +0100

    refactor: remove module compute resources from root file

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,7 +1,6 @@
 module "compute" {
   source              = "../../modules/compute"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+
 }
 
 module "networking" {
