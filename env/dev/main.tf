9123d7912e9250013ea7a82bd9198680e8bbafe5
Seyi Abiodun <cypher04>
2026-09-04 16:23:06 +0100

    update(ci): Add vm to infrastructure. Add command to export vm password at piepline runtime in gitlab yaml file

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,6 +1,15 @@
+// import the existing resource group into the state file
+
+# import {
+#   id = "/subscriptions/fe797b99-c79d-4fe3-baa0-091bacc96c3a/resourceGroups/cicd-resource-group"
+#   to = module.networking.cicd-resource-group
+# }
+
 module "compute" {
   source = "../../modules/compute"
-
+  location = var.location
+  vm_password = var.vm_password
+  subnet_ids = module.networking.subnet_ids[0]
 }
 
 module "networking" {
--- gitlab/variables.tf
+++ gitlab/variables.tf
@@ -1,35 +1,40 @@
 variable "tenant_id" {
-    description = "The Azure AD tenant ID."
-    type        = string
+  description = "The Azure AD tenant ID."
+  type        = string
 }
 
 variable "gitlab_token" {
-    description = "The GitLab personal access token."
-    type        = string
+  description = "The GitLab personal access token."
+  type        = string
 }
 
-variable "gitlab_base_url"{
-    description = "The GitLab instance URL."
-    type        = string
+variable "gitlab_base_url" {
+  description = "The GitLab instance URL."
+  type        = string
 }
 
 variable "gitlab_namespace" {
-    description = "The GitLab namespace (group or user) where the project will be created."
-    type        = string
+  description = "The GitLab namespace (group or user) where the project will be created."
+  type        = string
 }
 
 variable "gitlab_project_name" {
-    description = "The name of the GitLab project to be created."
-    type        = string
+  description = "The name of the GitLab project to be created."
+  type        = string
 }
 
 variable "default_branch" {
-    description = "The default branch for the GitLab project."
-    type        = string
-    default     = "main"
+  description = "The default branch for the GitLab project."
+  type        = string
+  default     = "main"
 }
 
 variable "subscription_id" {
-    description = "The Azure subscription ID."
-    type        = string
+  description = "The Azure subscription ID."
+  type        = string
+}
+
+variable "vm_password" {
+  description = "The password for the virtual machine."
+  type        = string
 }
