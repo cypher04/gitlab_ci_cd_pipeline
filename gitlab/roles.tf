6dafedbcb7351717fccfab175acde66852071671
Seyi Abiodun <cypher04>
2026-09-08 13:43:14 +0100

    edit(role): change contributor role scope from resource group to subscription

--- gitlab/roles.tf
+++ gitlab/roles.tf
@@ -6,8 +6,10 @@ resource "azurerm_role_assignment" "gitlab_api_access" {
   principal_id   = azuread_service_principal.gitlab_sp.object_id
   role_definition_name = "Contributor"
   // resource group scope
-  scope = "/subscriptions/${var.subscription_id}/resourceGroups/cicdprojectdev-rg"
+  scope = "/subscriptions/${var.subscription_id}"
 }
+
+
 
 // role for service principal to access storage account for Terraform state
 resource "azurerm_role_assignment" "gitlab_storage_access" {
